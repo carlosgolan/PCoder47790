2b2bf93873a23a7faa2f88765402f0407d3cf54a
Carlos Golan <cagmatrix@gmail.com>
2023-11-24 13:37:40 -0300

    no se

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+        
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    nacimiento = models.DateField(null=True, blank=True)
+    pais_origen = models.CharField(Pais, on_delete=models.SET_NULL, null=True, blank=True)
